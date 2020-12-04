bf71572334de9366e8a43091056d34cd8c83bc80
Nicholas Nicholson <nicholas.nicholson@hcl.com>
2020-12-04 17:19:04 -0600

    New Commit

--- src/filehandling/FileHandling.java
+++ src/filehandling/FileHandling.java
@@ -1,42 +1,91 @@
 package filehandling;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-
+import java.util.Scanner;
 
 public class FileHandling {
+	
+	public static void SystemOptions() // Give User options
+	{
+		System.out.println("1. [ Read File ]");
+		System.out.println("2. [ Display File ]");
+		System.out.println("3. [ Multiplication ]");
+		System.out.println("4. [ Division ]");
+		System.out.println("5: { Close Application }");
+	}
 
 	public static void ReadFile(String path, List<String> list) {
 
 		try {
 			File f = new File(path);
 			BufferedReader br = new BufferedReader(new FileReader(f));
-			String line = br.readLine();
 			
-			while (line != null) {
-				list.add(line);
-				line = br.readLine();
+			String line;
+			
+			while ((line = br.readLine()) != null) {
+				list.add(line); // Store for future use
+				//line = br.readLine();
+				// line = br.
 			}
-			
+
 			br.close();
 			System.out.println("Success: File read");
-			
-		} catch (IOException e) 
-		{
+
+		} catch (IOException e) {
 			System.out.println("Failure: Unable to read file.");
-
 		}
+
+	}
+
+	public static void DisplayFile(String path) //
+	{
+		try {
+			File f = new File(path);
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			
+			String line;
+			
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			br.close();
+			System.out.println("Success: File read");
+
+		} catch (IOException e) {
+			System.out.println("Failure: Unable to read file.");
+		}
+	}
+	public static void AppendFile(String path) 
+	{
+		String appendText = GetAppendText();
 		
+		try {
+			BufferedWriter br = new BufferedWriter(new FileWriter(path, true));
+			br.write(appendText);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	public static void WriteFile() {
-
-	}
-
-	public static void AppendFile() {
-
+	private static String GetAppendText() 
+	{
+		Scanner input = new Scanner(System.in);
+		String appendText = null;
+		
+		input.close();
+		
+		return appendText;
 	}
 
 }
--- src/filehandling/FileMain.java
+++ src/filehandling/FileMain.java
@@ -2,21 +2,43 @@ package filehandling;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileMain {
 
 	public static final String TESTFILE = "FILE.txt";
 	
+	
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		List<String> fileList = new ArrayList<>();
+		Scanner in = new Scanner(System.in);
+		List<String> fileContent = new ArrayList<>();
+		String choice = "0";
 		
-		//Get user's file path or use default file
-		//
-		FileHandling.ReadFile(TESTFILE, fileList);
+		System.out.println("Select an option: ");
+		FileHandling.SystemOptions();
 		
-
+		choice = in.nextLine();
+		
+		while (choice != "5") 
+		{
+			switch (choice) {
+			case "1":
+				FileHandling.ReadFile(TESTFILE, fileContent);
+				break;
+			case "2":
+				FileHandling.DisplayFile(TESTFILE);
+				break;
+			case "3":
+				
+				FileHandling.AppendFile(TESTFILE);
+				break;	
+			}
+			
+			FileHandling.SystemOptions();
+			choice = in.nextLine();
+		}
 
 	}
 
